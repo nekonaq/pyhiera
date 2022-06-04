8b7747b6dbdf2bcf9344bebaeb158119140c8ce3
Tatsuo Nakajyo <tnak@nekonaq.com>
2022-06-05 03:54:19 +0900

    Release 2.1.2205

--- pyhiera/settings.py
+++ pyhiera/settings.py
@@ -6,14 +6,21 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARNING)
 
 
-def load_settings_from_config(hiera_configs, **context):
+def load_settings_from_config(hiera_configs, namespace=None, **context):
     for confpath in hiera_configs if isinstance(hiera_configs, (list, tuple)) else (hiera_configs,):
         if not os.path.exists(confpath):
             continue
         logger.info("### config: {}".format(confpath))
-        # print("### config:", confpath)
         hiera = Hiera.load_data(confpath, context=context)
         hiera_data = hiera.flatten()
+        if namespace:
+            prefix = '{}_'.format(namespace.upper())
+            return {
+                key.replace(prefix, ''): val
+                for key, val in hiera_data.items()
+                if key.startswith(prefix) or key in ('ENVIRONMENT', 'CONFIG')
+            }
+
         return hiera_data
 
     logger.warning("# WARNING -- no config file found.")
